ddbf765364e1ced61643752633264504fb64d836
noolfj <firdavsjuraev183@gmail.com>
2023-10-07 09:19:38 +0500

    create laravel project jobstart

---
+++ resources/views/about.blade.php
@@ -0,0 +1,170 @@
+@extends('layout')
+
+@section('title')
+    Страница About
+@endsection
+
+@section('banner')
+ <div class="unit-5 overlay" style="background-image: url('images/hero_bg_2.jpg');">
+      <div class="container text-center">
+        <h2 class="mb-0">About</h2>
+        <p class="mb-0 unit-6"><a href="index.html">Home</a> <span class="sep">></span> <span>About</span></p>
+      </div>
+    </div>
+
+@endsection
+
+@section('content')
+
+    <div class="site-section border-top">
+      <div class="container">
+        <div class="row justify-content-center text-center mb-5">
+          <div class="col-md-6" data-aos="fade" >
+            <h2 class="text-black">Why Job<strong>start</strong> </h2>
+          </div>
+        </div>
+        <div class="row hosting">
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-4" data-aos="fade" data-aos-delay="100">
+
+            <div class="unit-3 h-100 bg-white">
+              
+              <div class="d-flex align-items-center mb-3 unit-3-heading">
+                <div class="unit-3-icon-wrap mr-4">
+                  <svg class="unit-3-svg" xmlns="http://www.w3.org/2000/svg" width="59px" height="68px">
+                    <path fill-rule="evenodd" stroke-width="2px" stroke-linecap="butt" stroke-linejoin="miter" fill="none" d="M29.000,66.000 L1.012,49.750 L1.012,17.250 L29.000,1.000 L56.988,17.250 L56.988,49.750 L29.000,66.000 Z"></path>
+                  </svg><span class="unit-3-icon icon fl-bigmug-line-portfolio23"></span>
+                </div>
+                <h2 class="h5">Search Millions of Jobs</h2>
+              </div>
+              <div class="unit-3-body">
+                <p>Lorem ipsum dolor sit amet consectetur is a nice adipisicing elita ssumenda a similique perferendis dolorem eos.</p>
+              </div>
+            </div>
+
+          </div>
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-4" data-aos="fade" data-aos-delay="200">
+            
+            <div class="unit-3 h-100 bg-white">
+              
+              <div class="d-flex align-items-center mb-3 unit-3-heading">
+                <div class="unit-3-icon-wrap mr-4">
+                  <svg class="unit-3-svg" xmlns="http://www.w3.org/2000/svg" width="59px" height="68px">
+                    <path fill-rule="evenodd" stroke-width="2px" stroke-linecap="butt" stroke-linejoin="miter" fill="none" d="M29.000,66.000 L1.012,49.750 L1.012,17.250 L29.000,1.000 L56.988,17.250 L56.988,49.750 L29.000,66.000 Z"></path>
+                  </svg><span class="unit-3-icon icon fl-bigmug-line-big104"></span>
+                </div>
+                <h2 class="h5">Location Search</h2>
+              </div>
+              <div class="unit-3-body">
+                <p>Lorem ipsum dolor sit amet consectetur is a nice adipisicing elita ssumenda a similique perferendis dolorem eos.</p>
+              </div>
+            </div>
+
+          </div>
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-4" data-aos="fade" data-aos-delay="300">
+            
+            <div class="unit-3 h-100 bg-white">
+              
+              <div class="d-flex align-items-center mb-3 unit-3-heading">
+                <div class="unit-3-icon-wrap mr-4">
+                  <svg class="unit-3-svg" xmlns="http://www.w3.org/2000/svg" width="59px" height="68px">
+                    <path fill-rule="evenodd" stroke-width="2px" stroke-linecap="butt" stroke-linejoin="miter" fill="none" d="M29.000,66.000 L1.012,49.750 L1.012,17.250 L29.000,1.000 L56.988,17.250 L56.988,49.750 L29.000,66.000 Z"></path>
+                  </svg><span class="unit-3-icon icon fl-bigmug-line-airplane86"></span>
+                </div>
+                <h2 class="h5">Top Careers</h2>
+              </div>
+              <div class="unit-3-body">
+                <p>Lorem ipsum dolor sit amet consectetur is a nice adipisicing elita ssumenda a similique perferendis dolorem eos.</p>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-4" data-aos="fade" data-aos-delay="400">
+
+            <div class="unit-3 h-100 bg-white">
+              
+              <div class="d-flex align-items-center mb-3 unit-3-heading">
+                <div class="unit-3-icon-wrap mr-4">
+                  <svg class="unit-3-svg" xmlns="http://www.w3.org/2000/svg" width="59px" height="68px">
+                    <path fill-rule="evenodd" stroke-width="2px" stroke-linecap="butt" stroke-linejoin="miter" fill="none" d="M29.000,66.000 L1.012,49.750 L1.012,17.250 L29.000,1.000 L56.988,17.250 L56.988,49.750 L29.000,66.000 Z"></path>
+                  </svg><span class="unit-3-icon icon fl-bigmug-line-user144"></span>
+                </div>
+                <h2 class="h5">Search Expert Candidates</h2>
+              </div>
+              <div class="unit-3-body">
+                <p>Lorem ipsum dolor sit amet consectetur is a nice adipisicing elita ssumenda a similique perferendis dolorem eos.</p>
+              </div>
+            </div>
+
+          </div>
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-4" data-aos="fade" data-aos-delay="500">
+            
+            <div class="unit-3 h-100 bg-white">
+              
+              <div class="d-flex align-items-center mb-3 unit-3-heading">
+                <div class="unit-3-icon-wrap mr-4">
+                  <svg class="unit-3-svg" xmlns="http://www.w3.org/2000/svg" width="59px" height="68px">
+                    <path fill-rule="evenodd" stroke-width="2px" stroke-linecap="butt" stroke-linejoin="miter" fill="none" d="M29.000,66.000 L1.012,49.750 L1.012,17.250 L29.000,1.000 L56.988,17.250 L56.988,49.750 L29.000,66.000 Z"></path>
+                  </svg><span class="unit-3-icon icon fl-bigmug-line-clipboard68"></span>
+                </div>
+                <h2 class="h5">Easy To Manage Jobs</h2>
+              </div>
+              <div class="unit-3-body">
+                <p>Lorem ipsum dolor sit amet consectetur is a nice adipisicing elita ssumenda a similique perferendis dolorem eos.</p>
+              </div>
+            </div>
+
+          </div>
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-4" data-aos="fade" data-aos-delay="600">
+            
+            <div class="unit-3 h-100 bg-white">
+              
+              <div class="d-flex align-items-center mb-3 unit-3-heading">
+                <div class="unit-3-icon-wrap mr-4">
+                  <svg class="unit-3-svg" xmlns="http://www.w3.org/2000/svg" width="59px" height="68px">
+                    <path fill-rule="evenodd" stroke-width="2px" stroke-linecap="butt" stroke-linejoin="miter" fill="none" d="M29.000,66.000 L1.012,49.750 L1.012,17.250 L29.000,1.000 L56.988,17.250 L56.988,49.750 L29.000,66.000 Z"></path>
+                  </svg><span class="unit-3-icon icon fl-bigmug-line-user143"></span>
+                </div>
+                <h2 class="h5">Online Reviews</h2>
+              </div>
+              <div class="unit-3-body">
+                <p>Lorem ipsum dolor sit amet consectetur is a nice adipisicing elita ssumenda a similique perferendis dolorem eos.</p>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    
+    <div class="site-blocks-cover" style="background-image: url(images/hero_bg_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+      <div class="container">
+        <div class="row row-custom align-items-center">
+          <div class="col-md-10">
+            <h1 class="mb-2 text-black w-75">Get 40% Less For <span class="font-weight-bold">New Employer</span> Job Posts</h1>
+            <p class="h3"><a href="#" class="btn btn-primary py-3 px-5">Post a Job</a></p>
+          </div>
+        </div>
+      </div>
+    </div>    
+    <div class="py-5 bg-primary">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h2 class="text-white h4 font-weihgt-normal mb-4">Subscribe Newsletter</h2>
+          </div>
+        </div>
+        <form action="" class="row">
+          <div class="col-md-9">
+            <input type="text" class="form-control border-0 mb-3 mb-md-0" placeholder="Enter Your Email">
+          </div>
+          <div class="col-md-3">
+            <input type="submit" value="Send" class="btn btn-dark btn-block" style="height: 45px;">  
+          </div>
+        </form>
+      </div>
+    </div>
+
+    @endsection
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\AboutController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('home');
+// });
+// Route::get('/', function () {
+//     return view('blog');
+// });
+// Route::get('/', function () {
+//     return view('about');
+// });
+
+
+Route::get('/index.html', [HomeController::class, 'home']);
+
+Route::get('/blog.html', [BlogController::class, 'blog']);
+
+Route::get('/about.html', [AboutController::class, 'about']);
+
+// Route::get('/contact', [ContactController::class, 'contact']);
+
