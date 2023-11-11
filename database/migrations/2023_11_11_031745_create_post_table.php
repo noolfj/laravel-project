fd9fcbcc0c71fc0216fe70515fe112c969fb2aef
noolfj <firdavsjuraev183@gmail.com>
2023-11-11 09:00:06 +0500

    Created migration, model % controller Post

--- database/migrations/2023_11_11_031745_create_post_table.php
+++ database/migrations/2023_11_11_031745_create_post_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('blog', function (Blueprint $table) {
+        Schema::create('post', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->longText('description')->nullable(); 
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('blog');
+        Schema::dropIfExists('post');
     }
 };
